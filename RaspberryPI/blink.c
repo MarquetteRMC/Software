4ee0903c2df1c619426a8a32dd2bbd01b993ccc7
8489faustn <nfaust1234@gmail.com>
2018-11-12 01:20:17 +0000

    Added reading and writing abilities. LED now toggles with push button

--- RaspberryPI/blink.c
+++ RaspberryPI/blink.c
@@ -1,12 +1,23 @@
+#include <stdio.h>
 #include <wiringPi.h>
+#include <stdbool.h>
+
 int main (void)
 {
   wiringPiSetup () ;
   pinMode (0, OUTPUT) ;
-  for (;;)
+  pinMode (3, INPUT);
+
+for (;;)
   {
-    digitalWrite (0, HIGH) ; delay (500) ;
-    digitalWrite (0,  LOW) ; delay (500) ;
+    digitalWrite (0, HIGH) ; //delay (500) ;
+   // digitalWrite (0,  LOW) ; delay (500) ;
+   if (digitalRead(3) == HIGH){
+	digitalWrite(0, LOW);
+	}
+   delay (500);
+
   }
+
   return 0 ;
 }
